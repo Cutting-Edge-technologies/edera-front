faebc0a206d3c75c923851cc4002cb79f3e944f0
UrUrZa <IshvarIndrovich@mail.ru>
2022-12-05 12:20:05 +0400

    added dummie obj

--- src/components/dummieObj.tsx
+++ src/components/dummieObj.tsx
@@ -2,6 +2,7 @@ import { IUser } from "./activate"
 import { IItem } from "./pair_info"
 import { IGroup, IService } from "./service"
 import { ICost, IServiceWithGroup, IStudentService } from "./student_service"
+import { ILesson, ISchaduleData} from "./timetable_table"
 
 export const dummieGroups: IGroup[] = [
   {
@@ -167,4 +168,90 @@ export const dummieStudentService: IStudentService[] = [
     id: 111,
     service: dummieServicesWithGroup[2]
   },
+]
+
+export const dummieLesson: ILesson = {
+  id:22,
+  pair_id: 33,
+  info: 'aaa',
+  name: 'bbb',
+  old_info: 'ccc',
+  repeat: 'ddd',
+  break_duration:"eee",
+  break_start:"fff",
+  date:"ggg",
+  duration_cost:"44",
+  hours:"55",
+  minutes:"66",
+  responsible:"77",
+  service:{
+    cost:88,
+    earn:99,
+    name:"qqq",
+    note_id:111
+  },
+  start_hour:"222",
+  start_minute:"333",
+  tz:{
+    hours:"+23",
+    id:"444",
+    name:"www"
+  },
+  zoom:"zzz"
+}
+
+export const dummieStrinds: string[] = [
+  "Lorem", "Ipsum", "John", "Dow"
+]
+
+export const dummieSchaduleData: ISchaduleData[] =[
+  {
+    color:"black",
+    day_of_week:6,
+    endDate:"28/12",
+    id:"aaa",
+    start_hour:"sss",
+    startDate:"ttt",
+    title:"Lorem Ipsum",
+    date:"12/15",
+    duration_cost:1500,
+    location:"Oslo",
+    teacher:"Norberd",
+    endDate_orig:"02/11",
+    startDate_orig:"25/09",
+    tz_name:"Oslo",
+  },
+  {
+    color:"white",
+    day_of_week:2,
+    endDate:"14/04",
+    id:"bb",
+    start_hour:"nnn",
+    startDate:"eee",
+    title:"John Dow",
+    date:"88/55",
+    duration_cost:200,
+    location:"London",
+    teacher:"Dobby",
+    endDate_orig:"77/11",
+    startDate_orig:"04/56",
+    tz_name:"London",
+  },
+  {
+    color:"green",
+    day_of_week:1,
+    endDate:"00/11",
+    id:"ccc",
+    start_hour:"qqq",
+    startDate:"rrr",
+    title:"Prow",
+    date:"22/99",
+    duration_cost:256054,
+    location:"Shmaganroge",
+    teacher:"Argent",
+    endDate_orig:"45/33",
+    startDate_orig:"22/15",
+    tz_name:"Shmaganroge",
+  },
+
 ]
