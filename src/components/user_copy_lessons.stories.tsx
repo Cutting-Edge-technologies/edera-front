8e10111e353c14e4e492f8bf59ea807a30160482
UrUrZa <IshvarIndrovich@mail.ru>
2022-12-05 16:01:12 +0400

    add user_copy_lessons stories

---
+++ src/components/user_copy_lessons.stories.tsx
@@ -0,0 +1,29 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { UserCopyLessons } from "./user_copy_lessons";
+import { dummieUsers} from "./dummieObj";
+
+export default {
+  title: 'Components/UserCopyLessons',
+  component: UserCopyLessons,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof UserCopyLessons>;
+
+const dummieDubleNumberArray: number[][] = [
+  [1,2,3,4],
+  [5,6,7,8],
+  [9,10,11,12]
+]
+
+
+export const ActivateStory: ComponentStory<any> = () => {
+  return (
+    <UserCopyLessons
+    token={""}
+    users={dummieUsers}
+    weeks_from = {dummieDubleNumberArray}
+    weeks_to ={dummieDubleNumberArray}
+    />
+  )
+}
