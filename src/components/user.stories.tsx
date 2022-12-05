c916089b2d7764dedf9ee3abad00b8945d11b06f
UrUrZa <IshvarIndrovich@mail.ru>
2022-12-05 19:56:09 +0400

    add user info stories

---
+++ src/components/user.stories.tsx
@@ -0,0 +1,23 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { UserInfo } from "./user";
+import { dummieGroups, dummieUserInfoUsers, dummieUserInfoTeachers, dummieUserInfoFamilies} from "./dummieObj";
+
+export default {
+  title: 'Components/UserInfo',
+  component: UserInfo,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof UserInfo>;
+
+export const ActivateStory: ComponentStory<any> = () => {
+  return (
+    <UserInfo
+    token={""}
+    managers={dummieGroups}
+    families={dummieUserInfoFamilies}
+    teachers={dummieUserInfoTeachers}
+    users={dummieUserInfoUsers}
+    />
+  )
+}
