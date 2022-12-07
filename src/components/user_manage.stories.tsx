6306b7e7d578185b5d9ff97150798984daa4285d
UrUrZa <IshvarIndrovich@mail.ru>
2022-12-07 18:15:50 +0400

    add user manage stories

---
+++ src/components/user_manage.stories.tsx
@@ -0,0 +1,25 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { AddUser } from "./user_manage";
+import { dummieStrinds, dummieServices, dummieAddUserChats, dummieAddUserPairs, dummieAddUserUsers } from "./dummieObj";
+
+export default {
+  title: 'Components/Add_User',
+  component: AddUser,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof AddUser>;
+
+
+export const ActivateStory: ComponentStory<any> = () => {
+  return (
+    <AddUser
+    token={""}
+    services={dummieServices}
+    responsibles={dummieStrinds}
+    chats={dummieAddUserChats}
+    pairs={dummieAddUserPairs}
+    users={dummieAddUserUsers}
+    />
+  )
+}
