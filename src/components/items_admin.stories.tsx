673dd68d941c60ff835e58d01a0c6f953af1204d
UrUrZa <ishvarIndrovich@mail.ru>
2022-12-13 18:08:55 +0400

    add groups servece stories

--- src/components/items_admin.stories.tsx
+++ src/components/items_admin.stories.tsx
@@ -7,8 +7,16 @@ export default {
   component: ItemsManage
 } as ComponentMeta<typeof ItemsManage>;
 
+const currencyFields = [dummieFelds[0]];
+
 export const Currency_5_7_Story: ComponentStory<any> = () => {
   return (
-    <ItemsManage token={""} fields={dummieFelds} items={dummieGroups} desc={{name:"Currency"}}/>
+    <ItemsManage token={""} fields={currencyFields} items={dummieGroups} desc={{name:"Currency"}}/>
+  )
+}
+
+export const ServiceGroups_5_8_Story: ComponentStory<any> = () => {
+  return (
+    <ItemsManage token={""} fields={dummieFelds} items={dummieGroups} desc={{name:"Service Groups"}}/>
   )
 }
