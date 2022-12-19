20396d2879442f05ada1593679d79b5ca8b69d16
UrUrZa <ishvarIndrovich@mail.ru>
2022-12-19 19:27:04 +0400

    add dummie signUp component

---
+++ src/components/signUp.tsx
@@ -0,0 +1,106 @@
+import { string } from "prop-types";
+import React from "react";
+import { InputGroupText } from "reactstrap";
+import { IHaveToken } from "../shared/typings";
+
+export interface IForm {
+  errors: string[];
+  username: {
+    name: string;
+    errors: string[];
+  };
+  password1: {
+    password1: string;
+    errors: string[];
+  };
+  password2: {
+    password2: string;
+    errors: string[];
+  };
+}
+
+export interface ISingUpProps{
+  crf_token: string;
+  signUp: () => void;
+  form: IForm;
+}
+
+export interface ISingUpState {
+  name: string;
+  password1: string;
+  password2: string;
+}
+
+ export class SingUp extends React.Component <ISingUpProps, ISingUpState> {
+  constructor(props: ISingUpProps) {
+    super(props);
+    this.state = 
+	    {
+        name: "",
+        password1: "",
+        password2: "",
+      };
+  }
+  render(): React.ReactNode {
+    return (
+      <div>
+        <h2>Sign up</h2>
+        <form>
+          { this.props.crf_token }
+          <div className="form-group">
+            <>
+              <label>Email</label>
+              { !!this.props.form.username.errors.length?
+                <div className="invalid-feedback">
+                  Данный email уже занят.
+                </div>: (<input type="text" onChange={(e)=>{e.target.value}}/>)
+              //value={form.username.name} 
+              }
+            </>
+          </div>
+          <div className="form-group">
+            <>
+              <label>Пароль</label>
+              { !!this.props.form.password1.errors.length?
+                <div className="invalid-feedback">
+                  Пароль должен:
+                  {this.props.form.password1.errors}
+                </div>: (<input type="password" onChange={(e)=>{e.target.value}}/>)
+                //<>{form.password1.password1}</>)
+              }
+            </>
+          </div>
+          <div className="form-group">
+            <>
+              <label>Повторите пароль</label>
+              {!!this.props.form.password2.errors.length?
+                <div className="invalid-feedback">
+                  {this.props.form.password2.errors}
+                </div>: (<input type="password" onChange={(e)=>{e.target.value}}/>)
+                //<>{form.password2.password2}</>
+              }
+            </>
+          </div>
+          <div className="form-group">
+            <select name="role">
+              <option value="moderator">Модератор</option>
+              <option value="student">Ученик</option>
+              <option value="teacher">Учитель</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Code</label>
+            <input name="code" type="password" className="" placeholder="for admin"/>
+            { !!this.props.form.username.errors.length&&
+            <div className="invalid-feedback">
+              Данный email уже занят.
+            </div>}
+           </div>
+          {this.props.form.errors}
+          <button onClick={this.props.signUp} className = "btn btn-lg btn-info">Sign up</button>
+          <button onClick={()=>console.log(name)}>Sign up info</button>
+        </form>
+      </div>
+    )
+  }
+}
