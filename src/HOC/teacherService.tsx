c34d61a5867e3061a84c9d56b9d97049b5e07fdb
UrUrZa <ishvarIndrovich@mail.ru>
2022-12-28 17:32:53 +0400

    fetch initial data services

--- src/HOC/teacherService.tsx
+++ src/HOC/teacherService.tsx
@@ -1,18 +1,13 @@
-import { dummieAll_dicts, dummieCosts, dummieStudentService, dummieUsers } from "../components/dummieObj";
 import { ITeacerServiceItemsManageProps, ItemsManage } from "../components/teacher_service";
-import { CommonHOCWrapper } from "../shared/commonHOC";
+import { CommonHOCWrapper, hostName } from "../shared/commonHOC";
 
 
 export class TeacherService extends CommonHOCWrapper<ITeacerServiceItemsManageProps> {
-  correspondingUrl = 'teacher/service/';
+
+  correspondingUrl =  `${hostName}teacher/service/`;
   fethInitialProps = async () => {
-    const initialData: ITeacerServiceItemsManageProps = {
-      token: '',
-      all_dicts: dummieAll_dicts,
-      users: dummieUsers,
-      costs: dummieCosts,
-      services: dummieStudentService
-    }
+    const response = await fetch(this.correspondingUrl);
+    const initialData = await response.json();
     return initialData;
   };
 
