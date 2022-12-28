0244e62396424c5fc0962734d70736e272f12034
UrUrZa <ishvarIndrovich@mail.ru>
2022-12-28 16:58:01 +0400

    remove coments

--- src/HOC/mentors.tsx
+++ src/HOC/mentors.tsx
@@ -2,14 +2,6 @@ import { CommonHOCWrapper, hostName } from "../shared/commonHOC";
 import { IItemsManageProps, ItemsManage} from "../components/mentors";
 
 export class Mentors extends CommonHOCWrapper<IItemsManageProps> {
-  // correspondingUrl = '/';
-  // fethInitialProps = async () => {
-  //   const initialData: IItemsManageProps = {
-  //     token: '',
-
-  //   }
-  //   return initialData;
-  // };
 
   correspondingUrl =  `${hostName}mentors/ `;
   fethInitialProps = async () => {
