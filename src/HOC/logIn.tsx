e417d0af004f260f60f79d0ff427b54ed8f33cd3
UrUrZa <ishvarIndrovich@mail.ru>
2023-01-05 19:01:50 +0400

    add logIn metod

--- src/HOC/logIn.tsx
+++ src/HOC/logIn.tsx
@@ -1,16 +1,23 @@
 
 import { dummieForm } from "../components/dummieObj";
 import { LogIn, ILogInProps } from "../components/logIn";
-import { CommonHOCWrapper } from "../shared/commonHOC";
+import { CommonHOCWrapper, hostName } from "../shared/commonHOC";
 
 
 export class LogInHOC extends CommonHOCWrapper<ILogInProps> {
-  correspondingUrl = 'login/';
-  fethInitialProps = async () => {
+  correspondingUrl =  `${hostName}login/ `;
+  fetchInitialProps = async () => {
     const initialData: ILogInProps = {
       csrf_token: "",
       form: dummieForm,
-      logIn: ()=>console.log("SignUp")
+      logIn: async (data)=> {
+        const fetchOptions = {
+          method: 'POST',
+          body: JSON.stringify(data),
+        } 
+        const newURL = "http://127.0.0.1:8000/api/v1/login/"
+        await fetch( newURL, fetchOptions);
+      }
     }
     return initialData;
   };
