95b8773afb51c64927a8be3991e60817c94d89be
UrUrZa <ishvarIndrovich@mail.ru>
2023-01-10 17:25:41 +0400

    add fetchInitialProps

--- src/HOC/services.tsx
+++ src/HOC/services.tsx
@@ -1,14 +1,33 @@
 
+import { useContext } from "react";
+import { AuthContext, IAuthContext } from "../App";
 import { IServiceProps, Service } from "../components/service";
 import { CommonHOCWrapper, hostName } from "../shared/commonHOC";
 
 
 export class Services extends CommonHOCWrapper<IServiceProps> {
-  
-  correspondingUrl =  `${hostName}services/ `;
+  static contextType = AuthContext;
+  correspondingUrl =  `${hostName}api/v1/services/ `;
   fetchInitialProps = async () => {
-    const response = await fetch(this.correspondingUrl);
-    const initialData = await response.json();
+    const {token} = this.context as IAuthContext;
+    console.log(token);
+    console.log(this.context);
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", `Token ${token}`);
+
+    var requestOptions = {
+      method: 'GET',
+      headers: myHeaders,
+    };
+    const response = await fetch(this.correspondingUrl, requestOptions);
+    const result = await response.text()
+    const responseData = JSON.parse(result)
+    const initialData: IServiceProps = {
+      all_dicts: responseData.all_dicts,
+      services: responseData.services,
+      token: token
+    };
+    console.log(initialData);
     return initialData;
   };
 
