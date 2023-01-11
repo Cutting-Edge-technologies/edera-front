f1659c3d7fa7c9ca0f1b5b6ef7bf79f98ee33422
UrUrZa <ishvarIndrovich@mail.ru>
2023-01-11 17:21:03 +0400

    add logout button

--- src/components/ButtonControl.tsx
+++ src/components/ButtonControl.tsx
@@ -1,4 +1,6 @@
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { resetToken } from "../slices/tokenSlice";
 
 export interface IButtonControlProps {
   title: string;
@@ -11,5 +13,17 @@ export const ButtonControl: React.FC<IButtonControlProps> = ({title, reference, 
     <Link to={ reference || '' }> 
       {title}
     </Link>
+  )
+}
+
+export const ButtonLogOut: React.FC<IButtonControlProps> = ({title, reference, isActive}) => {
+  const dispatch = useDispatch();
+  const logout = () =>{
+    dispatch(resetToken());
+  }
+  return (
+    <button onClick={logout} > 
+      {title}
+    </button>
   )
 }
