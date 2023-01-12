5066c6683a49a37c2d7158c03c40fc8303826c35
UrUrZa <ishvarIndrovich@mail.ru>
2023-01-12 15:56:03 +0400

    add logIn and logOut

--- src/commands/login.ts
+++ src/commands/login.ts
@@ -1,28 +1,37 @@
 import { createAction, PayloadAction } from "@reduxjs/toolkit";
 import { type } from "os";
 import { ILogInData } from "../components/logIn";
-import { tokenStateType } from "../store";
+import { tokenSelector } from "../selectors/token";
+import { hostName } from "../shared/commonHOC";
+import { tokenStore } from "../store";
 
 
-// const fetchToken = createAction(
-//   type: 'fetchToken',
-//   payload: 
+export const logIn = async (data:ILogInData) => {
+     
+  const bodyEncoded = new URLSearchParams();
+  bodyEncoded.append("username", data.username);
+  bodyEncoded.append("password", data.password);
+  console.warn(bodyEncoded);
+    const requestOptions = {
+      method: 'POST',
+      body: bodyEncoded,
+      redirect: "error" as any,
+    };
 
-// ) 
+  const response = await fetch(`${hostName}api/v1/login/`, requestOptions);
+  const result = await response.text();
+  const {token} = JSON.parse(result);
+  return (token);
+}
 
-
-
-// (state:tokenStateType, {payload}: PayloadAction<ILogInData>) => {
-//   const bodyEncoded = new URLSearchParams({...payload});
-//   const requestOptions = {
-//     method: 'POST',
-//     body: bodyEncoded,
-//     redirect: "error" as any,
-//   };
-
-//   const response = await fetch("http://127.0.0.1:8000/api/v1/login/", requestOptions);
-//   const result = await response.text();
-//   const {token} = JSON.parse(result);
-//   console.log(result);
-//   return ({value: token});
-// }
+export const logOut = async () => {
+  const token = tokenSelector(tokenStore.getState());   
+  var myHeaders = new Headers();
+  myHeaders.append("Authorization", `Token ${token}`);
+  const requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+  };
+  const response = await fetch(`${hostName}api/v1/logout/`, requestOptions);
+  }
+  
