7cef7398ac7e6b3e82206439aeff43255a17d588
UrUrZa <ishvarIndrovich@mail.ru>
2023-01-12 18:33:41 +0400

    add try blok

--- src/commands/API.ts
+++ src/commands/API.ts
@@ -2,15 +2,19 @@ import { tokenSelector } from "../selectors/token";
 import { tokenStore } from "../store";
 
 export const authorizedFetch = async (correspondingUrl:string) => {
-const token = tokenSelector(tokenStore.getState());
-var myHeaders = new Headers();
-myHeaders.append("Authorization", `Token ${token}`);
-var requestOptions = {
-  method: 'GET',
-  headers: myHeaders,
-};
-const response = await fetch(correspondingUrl, requestOptions);
-const result = await response.text()
-const data = JSON.parse(result)
-return({token, data});
+  const token = tokenSelector(tokenStore.getState());
+  var myHeaders = new Headers();
+  myHeaders.append("Authorization", `Token ${token}`);
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+  };
+  try{
+    const response = await fetch(correspondingUrl, requestOptions);
+    const result = await response.text()
+    const data = JSON.parse(result)
+    return({token, data});
+  } catch (e) {
+    console.error(e);
+  }
 }
