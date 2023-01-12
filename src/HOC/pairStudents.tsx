20beb2480e284be5758d9a65e12cda6fdbe7e2e5
UrUrZa <ishvarIndrovich@mail.ru>
2023-01-12 17:19:22 +0400

    add authorizedFetch initial props

--- src/HOC/pairStudents.tsx
+++ src/HOC/pairStudents.tsx
@@ -1,12 +1,22 @@
+import { authorizedFetch } from "../commands/API";
 import { IAddUserProps, AddUser } from "../components/user_manage";
 import { CommonHOCWrapper, hostName } from "../shared/commonHOC";
 
 
 export class PairStudents extends CommonHOCWrapper<IAddUserProps> {
-  correspondingUrl =  `${hostName}manage/ `;
+
+   correspondingUrl =  `${hostName}api/v1/manage/`;
   fetchInitialProps = async () => {
-    const response = await fetch(this.correspondingUrl);
-    const initialData = await response.json();
+    const responseData = await authorizedFetch(this.correspondingUrl)
+    const initialData: IAddUserProps = {
+      token: responseData.token,
+      users: responseData.data.users,
+      pairs: responseData.data.pairs,
+      services: responseData.data.services,
+      chats: responseData.data.chats,
+      responsibles: responseData.data.responsibles,
+      today: responseData.data.today
+    };
     return initialData;
   };
 
