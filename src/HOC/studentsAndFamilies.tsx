7808fbdc469cccdb8944d906f4ff6f4fc8e4d955
UrUrZa <ishvarIndrovich@mail.ru>
2023-01-12 18:38:29 +0400

    add authorizedFetch initial data

--- src/HOC/studentsAndFamilies.tsx
+++ src/HOC/studentsAndFamilies.tsx
@@ -1,13 +1,29 @@
 import { CommonHOCWrapper, hostName } from "../shared/commonHOC";
 import { IUserInfoProps, UserInfo } from "../components/user";
+import { authorizedFetch } from "../commands/API";
 
 export class StudentsAndFamilies extends CommonHOCWrapper<IUserInfoProps> {
 
-  correspondingUrl =  `${hostName}users/ `;
+  correspondingUrl =  `${hostName}api/v1/users/`;
   fetchInitialProps = async () => {
-    const response = await fetch(this.correspondingUrl);
-    const initialData = await response.json();
-    return initialData;
+    const responseData = await authorizedFetch(this.correspondingUrl)
+    if ( responseData ) {
+      const initialData: IUserInfoProps = {
+        token: responseData.token,
+        users: responseData.data.users,
+        families: responseData.data.families,
+        managers: responseData.data.managers,
+        teachers: responseData.data.teachers,
+      };
+      return initialData;
+    } else { 
+      return ({
+        token: '',
+        users: [],
+        families: [],
+        managers: [],
+        teachers: [],
+      })} 
   };
 
   RenderComponent = UserInfo;
