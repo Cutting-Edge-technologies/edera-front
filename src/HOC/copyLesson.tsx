0faea3389ad5782a6ddce3ebf99b46ee959e2243
UrUrZa <ishvarIndrovich@mail.ru>
2023-01-12 16:01:50 +0400

    add fetchInitialData

--- src/HOC/copyLesson.tsx
+++ src/HOC/copyLesson.tsx
@@ -1,13 +1,30 @@
 import { IUserCopyLessonsProps, UserCopyLessons } from "../components/user_copy_lessons";
+import { tokenSelector } from "../selectors/token";
 import { CommonHOCWrapper, hostName } from "../shared/commonHOC";
+import { tokenStore } from "../store";
 
 
 export class CopyLesson extends CommonHOCWrapper<IUserCopyLessonsProps> {
 
-  correspondingUrl =  `${hostName}manage/copy/`;
+  correspondingUrl =  `${hostName}api/v1/manage/copy/`;
   fetchInitialProps = async () => {
-    const response = await fetch(this.correspondingUrl);
-    const initialData = await response.json();
+    const token = tokenSelector(tokenStore.getState());
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", `Token ${token}`);
+    var requestOptions = {
+      method: 'GET',
+      headers: myHeaders,
+    };
+    const response = await fetch(this.correspondingUrl, requestOptions);
+    const result = await response.text()
+    const responseData = JSON.parse(result)
+    const initialData: IUserCopyLessonsProps = {
+      token: token,
+      users: responseData.users,
+      weeks_from: responseData.weeks_from,
+      weeks_to: responseData.weeks_to
+    };
+    console.log(initialData);
     return initialData;
   };
 
